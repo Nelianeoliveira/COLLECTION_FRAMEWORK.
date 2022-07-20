4f8a02247a629da63930a72b544b3a57befd4168
Neliane de Oliveira <92684306+Nelianeoliveira@users.noreply.github.com>
2022-07-20 15:54:22 -0300

    Initial commit

---
+++ src/br/com/collection/streamAPI/ExemploStream.java
@@ -0,0 +1,4 @@
+package br.com.collection.streamAPI;
+
+public class ExemploStream {
+}
---
+++ src/br/com/collection/streamAPI/ExerciciosStreamAPI.java
@@ -0,0 +1,56 @@
+package br.com.collection.streamAPI;
+
+import java.util.*;
+import java.util.function.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class ExerciciosStreamAPI {
+    public static void main(String[] args) {
+        List<String> numerosAleatorios =
+                Arrays.asList("1", "0", "4", "1", "2", "3", "9", "9", "6", "5");
+
+        List<Integer> numerosAleatoriosInteger = numerosAleatorios.stream()
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+        System.out.println("Ignore os 3 primeiros elementos da lista e imprima o restante:");
+        numerosAleatoriosInteger.stream()
+                .skip(3)
+                .forEach(System.out::println);
+
+        long countNumerosUnicos = numerosAleatoriosInteger.stream()
+                .distinct()
+                .count();
+        System.out.println("Retirando os números repetidos da lista, quantos números ficam? " + countNumerosUnicos);
+
+        System.out.print("Mostre o menor valor da lista: ");
+        numerosAleatoriosInteger.stream()
+                .mapToInt(Integer::intValue)
+                .min()
+                .ifPresent(System.out::println);
+
+        System.out.print("Mostre o maior valor da lista: ");
+        numerosAleatoriosInteger.stream()
+                .mapToInt(Integer::intValue)
+                .max()
+                .ifPresent(System.out::println);;
+
+        int somaDosNumerosPares = numerosAleatoriosInteger.stream()
+                .filter(i -> (i % 2 == 0))
+                .mapToInt(Integer::intValue)
+                .sum();
+        System.out.println("Pegue apenas os números ímpares e some: " + somaDosNumerosPares);
+
+        System.out.println("Mostre a lista na ordem númerica: ");
+        List<Integer> numerosOrdemNatural = numerosAleatoriosInteger.stream()
+                .sorted(Comparator.naturalOrder())
+                .collect(Collectors.toList());
+        System.out.println(numerosOrdemNatural);
+
+        System.out.println("Agrupe os valores ímpares múltiplos de 3 ou de 5:");
+//        dica: collect(Collectors.groupingBy(new Function())
+        Map<Boolean, List<Integer>> collectNumerosMultiplosDe3E5 = numerosAleatoriosInteger.stream()
+                .collect(Collectors.groupingBy(i -> (i % 3 == 0 || i % 5 == 0)));
+        System.out.println(collectNumerosMultiplosDe3E5);
+    }
+}
